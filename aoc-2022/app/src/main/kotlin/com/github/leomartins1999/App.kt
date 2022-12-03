4db5d9db0e63aa84ac36b6d53700be537651bd65
leomartins1999 <asmartins.leonardo1999@gmail.com>
2022-12-03 12:21:58 +0000

    AoC 2022 - Day 3

--- aoc-2022/app/src/main/kotlin/com/github/leomartins1999/App.kt
+++ aoc-2022/app/src/main/kotlin/com/github/leomartins1999/App.kt
@@ -5,14 +5,16 @@ package com.github.leomartins1999
 
 import com.github.leomartins1999.day1.Day1
 import com.github.leomartins1999.day2.Day2
+import com.github.leomartins1999.day3.Day3
 import java.io.File
 
 fun main() {
     day1()
     day2()
+    day3()
 }
 
-private fun day1(){
+private fun day1() {
     val day = Day1()
     val dayInput = getFileText("./input/day-1.txt")
 
@@ -24,7 +26,7 @@ private fun day1(){
     println("Day 1 (Part 2): $part2")
 }
 
-private fun day2(){
+private fun day2() {
     val day = Day2()
     val dayInput = getFileText("./input/day-2.txt")
 
@@ -34,6 +36,18 @@ private fun day2(){
     println("#".repeat(25))
     println("Day 2 (Part 1): $part1")
     println("Day 2 (Part 2): $part2")
+}
+
+private fun day3() {
+    val day = Day3()
+    val dayInput = getFileText("./input/day-3.txt")
+
+    val part1 = day.part1(dayInput)
+    val part2 = day.part2(dayInput)
+
+    println("#".repeat(25))
+    println("Day 3 (Part 1): $part1")
+    println("Day 3 (Part 2): $part2")
 }
 
 private fun getFileText(path: String) = File(path).readText()
