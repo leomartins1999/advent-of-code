be7946eaf22dedb96ac5bf2cdf8ea0a2f35f1612
leomartins1999 <asmartins.leonardo1999@gmail.com>
2023-11-30 17:23:35 +0100

    Add get_level function; Add unit tests

--- aoc-2023/src/days/day_1_2015.rs
+++ aoc-2023/src/days/day_1_2015.rs
@@ -1,3 +1,73 @@
-pub fn call() -> i32 {
-    return -1;
+pub fn solve() -> i32 {
+    return get_level("tbd");
+}
+
+fn get_level(input: &str) -> i32 {
+    let mut level = 0;
+
+    for c in input.chars() {
+        match c {
+            '(' => level += 1,
+            ')' => level -= 1,
+            _ => panic!("Illegal character '{c}' at '{input}'"),
+        }
+    }
+
+    return level;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(get_level("(())"), 0)
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(get_level("()()"), 0)
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(get_level("((("), 3)
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(get_level("(()(()("), 3)
+    }
+
+    #[test]
+    fn example_5() {
+        assert_eq!(get_level("))((((("), 3)
+    }
+
+    #[test]
+    fn example_6() {
+        assert_eq!(get_level("())"), -1)
+    }
+
+    #[test]
+    fn example_7() {
+        assert_eq!(get_level("))("), -1)
+    }
+
+    #[test]
+    fn example_8() {
+        assert_eq!(get_level(")))"), -3)
+    }
+
+    #[test]
+    fn example_9() {
+        assert_eq!(get_level(")())())"), -3)
+    }
+
+    #[test]
+    #[should_panic]
+    fn with_invalid_input() {
+        get_level("AHHHH");
+    }
 }
--- aoc-2023/src/main.rs
+++ aoc-2023/src/main.rs
@@ -1,5 +1,5 @@
 mod days;
 
 fn main() {
-    println!("(2015) Day 1: {}", days::day_1_2015::call())
+    println!("(2015) Day 1: {}", days::day_1_2015::solve())
 }
