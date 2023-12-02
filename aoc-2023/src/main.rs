f2bb93e18ad06779d08eb11c9e6018f2d49cb799
leomartins1999 <36698491+leomartins1999@users.noreply.github.com>
2023-12-02 21:59:24 +0000

    2023 - Day 2 (#38)
    * Add day 2 boilerplate
    
    * Add part 1 implementation
    
    * Make tests pass :)
    
    * Use common implementation for P1/P2

--- aoc-2023/src/main.rs
+++ aoc-2023/src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     println!("(2015) Day 1: {:?}", days::day_1_2015::solve());
     println!("<======================>");
     println!("(2023) Day 1: {:?}", days::day_1_2023::solve());
+    println!("(2023) Day 2: {:?}", days::day_2_2023::solve());
 }
