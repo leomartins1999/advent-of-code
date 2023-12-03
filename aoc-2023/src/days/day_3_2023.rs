9207b5b0decb522a636e47676a3a41c95c32d409
leomartins1999 <asmartins.leonardo1999@gmail.com>
2023-12-03 12:09:35 +0000

    Forgive father for I have sinned; Part 1 solution

--- aoc-2023/src/days/day_3_2023.rs
+++ aoc-2023/src/days/day_3_2023.rs
@@ -1,5 +1,7 @@
 use crate::utils;
 
+const RADIX: u32 = 10;
+
 pub fn solve() -> [u32; 2] {
     let input = utils::get_input(std::module_path!());
 
@@ -11,48 +13,159 @@ fn sum_part_numbers(input: &str) -> u32 {
 }
 
 fn build_schematic(input: &str) -> EngineSchematic {
-    let schema = input
+    let schema: Vec<Vec<char>> = input
         .split("\n")
+        .map(|line| line.trim())
         .map(|line| line.chars().collect())
         .collect();
 
-    return EngineSchematic { schema };
+    let width = schema[0].len();
+    let height = schema.len();
+
+    return EngineSchematic {
+        schema,
+        width,
+        height,
+    };
 }
 
+#[derive(Debug)]
 struct EngineSchematic {
     schema: Vec<Vec<char>>,
+    width: usize,
+    height: usize,
+}
+
+#[derive(PartialEq)]
+enum Direction {
+    Left,
+    Right,
+    Both,
 }
 
 impl EngineSchematic {
-    fn sum_part_numbers(&self) -> u32 {
-        let mut part_numbers = 0;
+    fn sum_part_numbers(&mut self) -> u32 {
+        self.filter_non_part_numbers();
 
-        for (y, line) in self.schema.iter().enumerate() {
-            for (x, _column) in line.iter().enumerate() {
-                let c = self.get(x, y);
+        return self.sum_numbers();
+    }
 
-                if is_symbol(c) {
-                    part_numbers += self.adjacent_part_numbers(x, y).iter().sum::<u32>();
+    fn filter_non_part_numbers(&mut self) {
+        for x in 0..self.width {
+            for y in 0..self.height {
+                if self.get(x, y).is_digit(RADIX) && !self.is_part_number(x, y, None) {
+                    self.remove(x, y)
+                }
+            }
+        }
+    }
+
+    fn is_part_number(&self, x: usize, y: usize, direction: Option<Direction>) -> bool {
+        if !self.get(x, y).is_digit(RADIX) {
+            return false;
+        }
+
+        let recursive_direction = direction.unwrap_or(Direction::Both);
+
+        println!(
+            "{x}{y} - {}, {}, {}",
+            self.get(x, y),
+            self.is_adjacent_to_symbol(x, y),
+            self.get(x, y).is_digit(RADIX)
+        );
+
+        if self.is_adjacent_to_symbol(x, y) {
+            return true;
+        }
+
+        if recursive_direction != Direction::Right
+            && x > 0
+            && self.is_part_number(x - 1, y, Some(Direction::Left))
+        {
+            return true;
+        }
+
+        if recursive_direction != Direction::Left
+            && x < self.width - 1
+            && self.is_part_number(x + 1, y, Some(Direction::Right))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    fn is_adjacent_to_symbol(&self, x: usize, y: usize) -> bool {
+        let starting_x = if x == 0 { 0 } else { x - 1 };
+        let ending_x = if x == self.width - 1 {
+            self.width - 1
+        } else {
+            x + 1
+        };
+
+        let starting_y = if y == 0 { 0 } else { y - 1 };
+        let ending_y = if y == self.height - 1 {
+            self.height - 1
+        } else {
+            y + 1
+        };
+
+        for other_x in starting_x..ending_x + 1 {
+            for other_y in starting_y..ending_y + 1 {
+                if self.is_symbol(other_x, other_y) {
+                    return true;
                 }
             }
         }
 
-        return part_numbers;
+        return false;
+    }
+
+    fn sum_numbers(&self) -> u32 {
+        return self
+            .schema
+            .iter()
+            .map(|line| {
+                line.iter()
+                    .map(|char| if char.is_digit(RADIX) { char } else { &'.' })
+                    .map(|char| char.to_owned())
+                    .collect::<Vec<char>>()
+            })
+            .map(|line| String::from_iter(line))
+            .flat_map(|line| {
+                line.split(".")
+                    .map(|str| str.to_owned())
+                    .collect::<Vec<String>>()
+            })
+            .map(|number_str| {
+                number_str
+                    .chars()
+                    .filter(|c| c.is_digit(RADIX))
+                    .collect::<Vec<char>>()
+            })
+            .filter(|number_chars| number_chars.len() > 0)
+            .map(|number_chars| String::from_iter(number_chars))
+            .map(|number_str| number_str.parse::<u32>().unwrap())
+            .map(|number| {
+                println!("{number}");
+                number
+            })
+            .sum();
+    }
+
+    fn is_symbol(&self, x: usize, y: usize) -> bool {
+        let c = self.get(x, y);
+
+        return !c.is_digit(RADIX) && c != '.';
     }
 
     fn get(&self, x: usize, y: usize) -> char {
         return self.schema[y][x];
     }
 
-    fn adjacent_part_numbers(&self, x: usize, y: usize) -> Vec<u32> {
-        
-        
-        return vec![0];
+    fn remove(&mut self, x: usize, y: usize) {
+        self.schema[y][x] = '.';
     }
-}
-
-fn is_symbol(c: char) -> bool {
-    return !c.is_alphanumeric() && c != '.';
 }
 
 #[cfg(test)]
