dc09381e21e259ff9a94bc5cca031ee6a077cfb7
leomartins1999 <asmartins.leonardo1999@gmail.com>
2023-12-03 13:16:01 +0000

    Add part 2... not that bad tbh

--- aoc-2023/src/days/day_3_2023.rs
+++ aoc-2023/src/days/day_3_2023.rs
@@ -1,15 +1,25 @@
 use crate::utils;
+use itertools::Itertools;
 
 const RADIX: u32 = 10;
+
+const GEAR: char = '*';
+const NOOP: char = '.';
+
+const GEAR_NUMBERS: usize = 2;
 
 pub fn solve() -> [u32; 2] {
     let input = utils::get_input(std::module_path!());
 
-    return [sum_part_numbers(&input), 0];
+    return [sum_part_numbers(&input), sum_gear_ratios(&input)];
 }
 
 fn sum_part_numbers(input: &str) -> u32 {
     return build_schematic(input).sum_part_numbers();
+}
+
+fn sum_gear_ratios(input: &str) -> u32 {
+    return build_schematic(input).sum_gear_ratios();
 }
 
 fn build_schematic(input: &str) -> EngineSchematic {
@@ -36,6 +46,12 @@ struct EngineSchematic {
     height: usize,
 }
 
+#[derive(PartialEq, Eq, Clone, Hash, Debug)]
+struct Position {
+    x: usize,
+    y: usize,
+}
+
 #[derive(PartialEq)]
 enum Direction {
     Left,
@@ -48,6 +64,75 @@ impl EngineSchematic {
         self.filter_non_part_numbers();
 
         return self.sum_numbers();
+    }
+
+    fn sum_gear_ratios(&self) -> u32 {
+        let mut sum = 0;
+
+        for x in 0..self.width {
+            for y in 0..self.height {
+                if self.get(x, y) == GEAR {
+                    let adjacent_numbers = self.get_adjacent_numbers(x, y);
+
+                    if adjacent_numbers.len() == GEAR_NUMBERS {
+                        println!("{:?}", adjacent_numbers);
+
+                        sum += adjacent_numbers.iter().fold(1, |sum, val| sum * val);
+                    }
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    fn get_adjacent_numbers(&self, x: usize, y: usize) -> Vec<u32> {
+        return self
+            .adjacent_positions(x, y)
+            .iter()
+            .filter(|pos| self.get_position(pos).is_digit(RADIX))
+            .map(|pos| self.get_number_beggining_position(&pos))
+            .collect_vec()
+            .iter()
+            .unique()
+            .map(|pos| self.get_number(&pos))
+            .collect_vec();
+    }
+
+    fn get_number_beggining_position(&self, pos: &Position) -> Position {
+        println!("get_number_beggining_position: {:?}", pos);
+
+        let mut x = pos.x;
+        let y = pos.y;
+
+        while x > 0 && self.get_position(&Position { x: x - 1, y }).is_digit(RADIX) {
+            x -= 1;
+        }
+
+        println!("-> {x},{y}");
+
+        return Position { x, y };
+    }
+
+    fn get_number(&self, beggining_pos: &Position) -> u32 {
+        println!("#get_number: {:?}", beggining_pos);
+
+        let mut acc = 0;
+
+        let mut x = beggining_pos.x;
+        let y = beggining_pos.y;
+
+        while x < self.width && self.get_position(&Position { x, y }).is_digit(RADIX) {
+            println!("{}", self.get_position_digit(&Position { x, y }));
+
+            acc *= 10;
+            acc += self.get_position_digit(&Position { x, y });
+            x += 1
+        }
+
+        println!("-> {acc}");
+
+        return acc;
     }
 
     fn filter_non_part_numbers(&mut self) {
@@ -96,6 +181,15 @@ impl EngineSchematic {
     }
 
     fn is_adjacent_to_symbol(&self, x: usize, y: usize) -> bool {
+        return self
+            .adjacent_positions(x, y)
+            .iter()
+            .any(|pos| self.is_symbol(pos));
+    }
+
+    fn adjacent_positions(&self, x: usize, y: usize) -> Vec<Position> {
+        let mut res = vec![];
+
         let starting_x = if x == 0 { 0 } else { x - 1 };
         let ending_x = if x == self.width - 1 {
             self.width - 1
@@ -112,13 +206,14 @@ impl EngineSchematic {
 
         for other_x in starting_x..ending_x + 1 {
             for other_y in starting_y..ending_y + 1 {
-                if self.is_symbol(other_x, other_y) {
-                    return true;
-                }
+                res.push(Position {
+                    x: other_x,
+                    y: other_y,
+                })
             }
         }
 
-        return false;
+        return res;
     }
 
     fn sum_numbers(&self) -> u32 {
@@ -127,8 +222,13 @@ impl EngineSchematic {
             .iter()
             .map(|line| {
                 line.iter()
-                    .map(|char| if char.is_digit(RADIX) { char } else { &'.' })
-                    .map(|char| char.to_owned())
+                    .map(|char| {
+                        if char.is_digit(RADIX) {
+                            char.to_owned()
+                        } else {
+                            NOOP
+                        }
+                    })
                     .collect::<Vec<char>>()
             })
             .map(|line| String::from_iter(line))
@@ -153,38 +253,51 @@ impl EngineSchematic {
             .sum();
     }
 
-    fn is_symbol(&self, x: usize, y: usize) -> bool {
-        let c = self.get(x, y);
+    fn is_symbol(&self, pos: &Position) -> bool {
+        let c = self.get(pos.x, pos.y);
 
-        return !c.is_digit(RADIX) && c != '.';
+        return !c.is_digit(RADIX) && c != NOOP;
     }
 
     fn get(&self, x: usize, y: usize) -> char {
         return self.schema[y][x];
     }
 
+    fn get_position(&self, pos: &Position) -> char {
+        return self.schema[pos.y][pos.x];
+    }
+
+    fn get_position_digit(&self, pos: &Position) -> u32 {
+        return self.schema[pos.y][pos.x].to_digit(RADIX).unwrap();
+    }
+
     fn remove(&mut self, x: usize, y: usize) {
-        self.schema[y][x] = '.';
+        self.schema[y][x] = NOOP;
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::sum_part_numbers;
+    use super::*;
+
+    const INPUT: &str = r#"467..114..
+    ...*......
+    ..35..633.
+    ......#...
+    617*......
+    .....+.58.
+    ..592.....
+    ......755.
+    ...$.*....
+    .664.598.."#;
 
     #[test]
-    fn example_1() {
-        let input = r#"467..114..
-        ...*......
-        ..35..633.
-        ......#...
-        617*......
-        .....+.58.
-        ..592.....
-        ......755.
-        ...$.*....
-        .664.598.."#;
+    fn part_1() {
+        assert_eq!(sum_part_numbers(INPUT), 4361)
+    }
 
-        assert_eq!(sum_part_numbers(input), 4361)
+    #[test]
+    fn part_2() {
+        assert_eq!(sum_gear_ratios(INPUT), 467835)
     }
 }
