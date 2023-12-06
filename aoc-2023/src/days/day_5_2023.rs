245a3f077e8c6d6413e518959f656d696a96e3d6
Leonardo Martins <lmartins@salsify.com>
2023-12-06 09:30:55 +0000

    Add part 2; I shall pay for my sins

--- aoc-2023/src/days/day_5_2023.rs
+++ aoc-2023/src/days/day_5_2023.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 use crate::utils;
@@ -5,7 +7,103 @@ use crate::utils;
 pub fn solve() -> [u64; 2] {
     let input = utils::get_input(std::module_path!());
 
-    return [get_lowest_location_number(&input), 0];
+    return [
+        get_lowest_location_number(&input),
+        get_lowest_location_p2(&input),
+    ];
+}
+
+fn get_lowest_location_p2(input: &str) -> u64 {
+    let mut lines = input.split("\n").map(|line| line.trim()).peekable();
+
+    let mut seeds = get_seeds_map(lines.next().unwrap());
+
+    while lines.peek().is_some() {
+        while !lines.next().unwrap().contains("map:") {
+            // noop
+        }
+
+        let mut mappings = Vec::new();
+
+        loop {
+            let line = lines.next().unwrap_or("");
+
+            if line.is_empty() {
+                break;
+            }
+
+            mappings.push(build_mapping(line))
+        }
+
+        seeds = apply_mappings_to_map(seeds, mappings);
+
+        print!("{:#?}", seeds);
+    }
+
+    return seeds.iter().map(|range| range.start).min().unwrap();
+}
+
+fn get_seeds_map(seeds_line: &str) -> Vec<SeedRange> {
+    return seeds_line
+        .split(":")
+        .nth(1)
+        .unwrap()
+        .trim()
+        .split(" ")
+        .chunks(2)
+        .into_iter()
+        .map(|mut chunk| {
+            let start = chunk.nth(0).unwrap().parse().unwrap();
+            let length = chunk.nth(0).unwrap().parse().unwrap();
+
+            return SeedRange { start, length };
+        })
+        .collect_vec();
+}
+
+fn apply_mappings_to_map(seeds_map: Vec<SeedRange>, mappings: Vec<Mapping>) -> Vec<SeedRange> {
+    return seeds_map
+        .iter()
+        .flat_map(|seed_range| explode_range(seed_range, &mappings))
+        .collect_vec();
+}
+
+fn explode_range(seed_range: &SeedRange, mappings: &Vec<Mapping>) -> Vec<SeedRange> {
+    println!("Exploding mappings for {:?}", seed_range);
+
+    let range = (seed_range.start..seed_range.start + seed_range.length - 1);
+
+    println!("* Range: {:#?}", range);
+
+    let applicable_mappings = mappings.iter().filter(|m| {
+        let mapping_range = m.source_start..m.source_start + m.range_length;
+
+        return range.start <= mapping_range.end && range.end >= mapping_range.start
+    }).collect_vec();
+
+    if applicable_mappings.is_empty() {
+        return vec![seed_range.clone()];
+    }
+
+    return applicable_mappings.iter()
+        .map(|m| {
+            println!("-> Intersects with {:?}", m);
+
+            let mapping_range = m.source_start..m.source_start + m.range_length;
+
+            let start_idx = if range.start > m.source_start { range.start - m.source_start } else { 0 } ;
+            let end_idx = u64::min(range.end - mapping_range.start, mapping_range.end - mapping_range.start) ;
+
+            let start = m.destination_start + start_idx;
+            let end = m.destination_start + end_idx;
+
+            println!("Destination Start: {start}, End: {end}");
+
+            let length = end - start;
+            
+            return SeedRange { start, length };
+        })
+        .collect_vec();
 }
 
 fn get_lowest_location_number(input: &str) -> u64 {
@@ -49,6 +147,12 @@ fn get_seeds(seeds_line: &str) -> Vec<u64> {
         .split(" ")
         .map(|seed_str| seed_str.parse::<u64>().unwrap())
         .collect_vec();
+}
+
+#[derive(Clone, Debug)]
+struct SeedRange {
+    start: u64,
+    length: u64,
 }
 
 fn build_mapping(mapping_line: &str) -> Mapping {
@@ -145,6 +249,6 @@ mod tests {
 
     #[test]
     fn part_2() {
-        // assert_eq!(get_lowest_location_number(INPUT), 35)
+        assert_eq!(get_lowest_location_p2(INPUT), 46)
     }
 }
