533760990b17a42826e9913c8fc544b00e27051a
Leonardo Martins <lmartins@salsify.com>
2024-12-01 03:52:19 +0000

    Initial commit

---
+++ aoc-2024/app/src/main/kotlin/com/leomartins1999/aoc/day1/Day1.kt
@@ -0,0 +1,5 @@
+package com.leomartins1999.aoc.day1
+
+class Day1 {
+    fun part1() = true
+}
---
+++ aoc-2024/app/src/test/kotlin/com/leomartins1999/aoc/day1/Day1Test.kt
@@ -0,0 +1,11 @@
+package com.leomartins1999.aoc.day1
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class Day1Test {
+    @Test
+    fun returnsTrue() {
+        assertTrue { Day1().part1() }
+    }
+}
