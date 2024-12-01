5fc766c6655c2e7c1ac6f4c68dfefa4b090babc9
Leonardo Martins <lmartins@salsify.com>
2024-12-01 13:07:07 +0000

    Solve part 1

--- aoc-2024/app/src/main/kotlin/com/leomartins1999/aoc/day1/Day1.kt
+++ aoc-2024/app/src/main/kotlin/com/leomartins1999/aoc/day1/Day1.kt
@@ -1,5 +1,50 @@
 package com.leomartins1999.aoc.day1
 
-class Day1 {
-    fun part1() = true
+import kotlin.math.absoluteValue
+
+fun main() {
+    val input = Day1::class.java.getResource("/inputs/day1.txt")!!.readText()
+    val part1Result = Day1(input).part1()
+
+    println("Part 1: $part1Result")
+}
+
+class Day1(
+    private val input: String
+) {
+    fun part1(): Int {
+        val lists = parseInput()
+        val comparisonResults = compareLists(lists)
+
+        return comparisonResults.sum()
+    }
+
+    private fun parseInput(): List<List<Int>> {
+        val lines = input
+            .split("\n")
+            .filter { it.isNotBlank() }
+            .map { parseLine(it) }
+
+        val nrOfLists = lines.first().size
+
+        return (0 until nrOfLists)
+            .map { listIdx ->
+                val elems = lines.map { it[listIdx] }
+
+                elems.sorted()
+            }
+    }
+
+    private fun parseLine(line: String): List<Int> {
+        return line
+            .split(" ")
+            .filter { it.isNotBlank() }
+            .map { it.toInt() }
+    }
+
+    private fun compareLists(lists: List<List<Int>>): List<Int> {
+        val (first, second) = lists
+
+        return first.zip(second) { firstValue, secondValue -> (firstValue - secondValue).absoluteValue }
+    }
 }
--- aoc-2024/app/src/test/kotlin/com/leomartins1999/aoc/day1/Day1Test.kt
+++ aoc-2024/app/src/test/kotlin/com/leomartins1999/aoc/day1/Day1Test.kt
@@ -1,11 +1,20 @@
 package com.leomartins1999.aoc.day1
 
 import kotlin.test.Test
-import kotlin.test.assertTrue
+import kotlin.test.assertEquals
 
 class Day1Test {
+    private val input = """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+    """.trimIndent()
+
     @Test
-    fun returnsTrue() {
-        assertTrue { Day1().part1() }
+    fun day1example() {
+        assertEquals(11, Day1(input).part1())
     }
 }
