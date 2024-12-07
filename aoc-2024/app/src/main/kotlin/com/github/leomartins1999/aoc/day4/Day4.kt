782a83a4197ec5109596b1d672839a7fb5aa1e61
Leonardo Martins <lmartins@salsify.com>
2024-12-07 01:14:01 +0000

    Add part 2

--- aoc-2024/app/src/main/kotlin/com/github/leomartins1999/aoc/day4/Day4.kt
+++ aoc-2024/app/src/main/kotlin/com/github/leomartins1999/aoc/day4/Day4.kt
@@ -3,25 +3,44 @@ package com.github.leomartins1999.aoc.day4
 import com.github.leomartins1999.aoc.Day
 
 class Day4(private val input: String) : Day {
-    override fun part1(): Any {
-        return parseInput().count("XMAS")
+    override fun part1(): Int {
+        return parseInput().countXMASs()
+    }
+
+    override fun part2(): Int {
+        return parseInput().countMASXs()
     }
 
     private fun parseInput(): WordSearcher {
-        val lines = input
-            .lines()
-            .map { it.toList() }
-            .filter { it.isNotEmpty() }
+        val lines =
+            input
+                .lines()
+                .map { it.toList() }
+                .filter { it.isNotEmpty() }
 
         return WordSearcher(lines)
     }
 }
 
 class WordSearcher(private val lines: List<List<Char>>) {
-    fun count(word: String): Int {
+    private val mas = "MAS".toList()
+
+    fun countXMASs(): Int {
         val lines = getHorizontalLines() + getVerticalLines() + getDiagonalLines()
 
-        return lines.fold(0) { acc, line -> acc + line.countOccurrences(word) }
+        return lines.fold(0) { acc, line -> acc + line.countXMASs() }
+    }
+
+    fun countMASXs(): Int {
+        var res = 0
+
+        (1 until width() - 1).forEach { x ->
+            (1 until height() - 1).forEach { y ->
+                if (isMASX(x, y)) res++
+            }
+        }
+
+        return res
     }
 
     private fun getHorizontalLines(): List<Line> {
@@ -40,13 +59,18 @@ class WordSearcher(private val lines: List<List<Char>>) {
         val topLeftToBottomRight = (0 until width()).map { x -> diagonalStartingFrom(x, 0) }
         val leftToBottomRight = (1 until height()).map { y -> diagonalStartingFrom(0, y) }
 
-        val bottomLeftToTopRight = (0 until width()).map { x -> diagonalStartingFrom(x, height() - 1, goingDown = false) }
+        val bottomLeftToTopRight =
+            (0 until width()).map { x -> diagonalStartingFrom(x, height() - 1, goingDown = false) }
         val leftToToRight = (0 until height() - 1).map { y -> diagonalStartingFrom(0, y, goingDown = false) }
 
         return topLeftToBottomRight + leftToBottomRight + bottomLeftToTopRight + leftToToRight
     }
 
-    private fun diagonalStartingFrom(x: Int, y: Int, goingDown: Boolean = true): Line {
+    private fun diagonalStartingFrom(
+        x: Int,
+        y: Int,
+        goingDown: Boolean = true,
+    ): Line {
         val chars = mutableListOf<Char>()
 
         var currX = x
@@ -66,24 +90,43 @@ class WordSearcher(private val lines: List<List<Char>>) {
         return Line(chars)
     }
 
-    private fun isValid(x: Int, y: Int) = x in 0 until width() && y in 0 until height()
+    private fun isMASX(
+        x: Int,
+        y: Int,
+    ): Boolean {
+        val topToBot = listOf(valueOf(x - 1, y - 1), valueOf(x, y), valueOf(x + 1, y + 1))
+        val botToTop = listOf(valueOf(x - 1, y + 1), valueOf(x, y), valueOf(x + 1, y - 1))
 
-    private fun valueOf(x: Int, y: Int) = lines[y][x]
+        return (topToBot == mas || topToBot == mas.reversed()) && (botToTop == mas || botToTop == mas.reversed())
+    }
+
+    private fun isValid(
+        x: Int,
+        y: Int,
+    ) = x in 0 until width() && y in 0 until height()
+
+    private fun valueOf(
+        x: Int,
+        y: Int,
+    ) = lines[y][x]
 
     private fun height() = lines.size
+
     private fun width() = lines.first().size
 }
 
 class Line(private val chars: List<Char>) {
-    fun countOccurrences(word: String): Int {
+    private val xmas = "XMAS"
+
+    fun countXMASs(): Int {
         var res = 0
 
         val currentChars = mutableListOf<Char>()
         chars.forEach { char ->
             currentChars.addLast(char)
-            if (currentChars.size > word.length) currentChars.removeFirst()
+            if (currentChars.size > xmas.length) currentChars.removeFirst()
 
-            if (currentChars == word.toList() || currentChars == word.reversed().toList()) {
+            if (currentChars == xmas.toList() || currentChars == xmas.reversed().toList()) {
                 res++
             }
         }
--- aoc-2024/app/src/test/kotlin/com/github/leomartins1999/aoc/day4/Day4Test.kt
+++ aoc-2024/app/src/test/kotlin/com/github/leomartins1999/aoc/day4/Day4Test.kt
@@ -1,24 +1,30 @@
 package com.github.leomartins1999.aoc.day4
 
-import org.junit.jupiter.api.Assertions.*
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class Day4Test {
-    @Test
-    fun part1Example() {
-        val input = """
-            MMMSXXMASM
-            MSAMXMSMSA
-            AMXSXMAAMM
-            MSAMASMSMX
-            XMASAMXAMM
-            XXAMMXXAMA
-            SMSMSASXSS
-            SAXAMASAAA
-            MAMMMXMMMM
-            MXMXAXMASX
+    private val input =
+        """
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
         """.trimIndent()
 
+    @Test
+    fun part1Example() {
         assertEquals(18, Day4(input).part1())
+    }
+
+    @Test
+    fun part2Example() {
+        assertEquals(9, Day4(input).part2())
     }
 }
