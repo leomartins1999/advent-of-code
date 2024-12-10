bd128ee17eebfaa881486634dbb89e241c37d3dd
Leonardo Martins <lmartins@salsify.com>
2024-12-10 18:33:50 +0000

    Add part 1

--- aoc-2024/app/src/main/kotlin/com/github/leomartins1999/aoc/day9/Day9.kt
+++ aoc-2024/app/src/main/kotlin/com/github/leomartins1999/aoc/day9/Day9.kt
@@ -3,10 +3,12 @@ package com.github.leomartins1999.aoc.day9
 import com.github.leomartins1999.aoc.Day
 
 class Day9(private val input: String) : Day {
-    override fun part1(): Int {
+    override fun part1(): Long {
         val diskMap = parseInput()
 
-        return diskMap.compact().checksum()
+        diskMap.compact()
+
+        return diskMap.checksum()
     }
 
     private fun parseInput(): DiskMap {
@@ -34,21 +36,47 @@ class Day9(private val input: String) : Day {
 }
 
 class DiskMap(private val blocks: List<Block>) {
-    fun compact(): List<Block> {
-        return listOf()
-    }
-}
+    fun compact() {
+        var startPtr = 0
+        var endPtr = blocks.size - 1
 
-fun List<Block>.checksum(): Int {
-    return this
-        .flatMap(Block::value)
-        .mapIndexed { index, value -> index * value }
-        .sum()
+        while (true) {
+            val startBlock = blocks[startPtr]
+            val endBlock = blocks[endPtr]
+
+            if (startPtr >= endPtr) break
+
+            if (startBlock.isFull()) {
+                startPtr++
+                continue
+            }
+            if (endBlock.isEmpty()) {
+                endPtr--
+                continue
+            }
+
+            val toMove = mutableListOf<Int>()
+            while (toMove.size < startBlock.freeSpace() && !endBlock.isEmpty()) {
+                toMove.add(endBlock.pop())
+            }
+
+            toMove.forEach(startBlock::push)
+        }
+    }
+
+    fun checksum(): Long {
+        return blocks
+            .flatMap(Block::value)
+            .mapIndexed { index, value -> index.toLong() * value }
+            .sum()
+    }
 }
 
 data class Block(val length: Int, val values: MutableList<Int> = mutableListOf()) {
     fun push(value: Int) = values.addLast(value)
     fun pop() = values.removeLast()
     fun isFull() = values.size == length
+    fun isEmpty() = values.isEmpty()
+    fun freeSpace() = length - values.size
     fun value() = values
 }
