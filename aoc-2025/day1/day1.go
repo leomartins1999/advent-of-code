6bf806113e0b217ee817d0c4aff13f8160c4804a
leomartins1999 <36698491+leomartins1999@users.noreply.github.com>
2025-12-03 13:12:57 +0000

    AOC 2025 - Day 1 (#57)

---
+++ aoc-2025/day1/day1.go
@@ -0,0 +1,99 @@
+package Day1
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+type Day1 struct{ InputFilePath string }
+
+type Instruction struct {
+	Direction string
+	Times     int
+}
+
+type Dial struct {
+	Location         int
+	ExactZeroCounter int
+	ZeroCounter      int
+}
+
+func (d *Day1) SolvePart1() any {
+	instructions := parseInput(d.InputFilePath)
+	dial := &Dial{Location: 50}
+
+	for _, instruction := range instructions {
+		dial.Move(instruction)
+	}
+
+	return dial.ExactZeroCounter
+}
+
+func (d *Day1) SolvePart2() any {
+	instructions := parseInput(d.InputFilePath)
+	dial := &Dial{Location: 50}
+
+	for _, instruction := range instructions {
+		dial.Move(instruction)
+	}
+
+	return dial.ZeroCounter
+}
+
+func (d *Dial) Move(instruction Instruction) {
+	// log.Printf("Moving %s %d from %d", instruction.Direction, instruction.Times, d.Location)
+
+	switch instruction.Direction {
+	case "L":
+		d.Location -= instruction.Times
+	case "R":
+		d.Location += instruction.Times
+	}
+
+	for {
+		if d.Location < 0 {
+			d.Location += 100
+			d.ZeroCounter++
+		} else if d.Location >= 100 {
+			d.Location -= 100
+			d.ZeroCounter++
+		} else {
+			break
+		}
+	}
+
+	if d.Location == 0 {
+		d.ExactZeroCounter++
+	}
+
+	// log.Printf("New location: %d", d.Location)
+}
+
+func parseInput(filePath string) []Instruction {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var instructions []Instruction
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		instructionText := scanner.Text()
+
+		direction := instructionText[:1]
+		times, err := strconv.Atoi(instructionText[1:])
+		if err != nil {
+			panic(err)
+		}
+
+		instructions = append(instructions, Instruction{Direction: direction, Times: times})
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return instructions
+}
