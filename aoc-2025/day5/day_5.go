dac685e107889b34264c5dec7aa8c02528e32755
Leonardo Martins <lmartins@salsify.com>
2025-12-05 15:59:49 +0000

    Day 5

---
+++ aoc-2025/day5/day_5.go
@@ -0,0 +1,122 @@
+package Day5
+
+import (
+	Utils "aoc-2025/utils"
+	"slices"
+	"strings"
+)
+
+type Day5 struct{ InputFilePath string }
+
+type IngredientIDRange struct{ Start, End int }
+
+func (d *Day5) SolvePart1() any {
+	ranges, ids := parseInput(d.InputFilePath)
+
+	// log.Printf("Parsed Ranges: %+v", ranges)
+	// log.Printf("Parsed IDs: %+v", ids)
+
+	cnt := 0
+	for _, id := range ids {
+		if isFreshIngredient(id, ranges) {
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
+func (d *Day5) SolvePart2() any {
+	ranges, _ := parseInput(d.InputFilePath)
+
+	reducedRanges := reduceRanges(ranges)
+
+	return countIngredientsInRanges(reducedRanges)
+}
+
+func (r IngredientIDRange) OverlapsWith(other IngredientIDRange) bool {
+	return r.Start <= other.End && other.Start <= r.End
+}
+
+func (r IngredientIDRange) MergeWith(other IngredientIDRange) IngredientIDRange {
+	return IngredientIDRange{
+		Start: min(r.Start, other.Start),
+		End:   max(r.End, other.End),
+	}
+}
+
+func reduceRanges(ranges []IngredientIDRange) []IngredientIDRange {
+	result := []IngredientIDRange{}
+
+	for _, r := range ranges {
+		merged := false
+
+		for idx, mergedRange := range result {
+			if r.OverlapsWith(mergedRange) {
+				newMergedRange := r.MergeWith(mergedRange)
+				// log.Printf("Merging range %+v with %+v to %+v", r, mergedRange, newMergedRange)
+				result[idx] = newMergedRange
+				merged = true
+				break
+			}
+		}
+
+		if !merged {
+			// log.Printf("Adding new range %+v", r)
+			result = append(result, r)
+		}
+	}
+
+	return result
+}
+
+func countIngredientsInRanges(ranges []IngredientIDRange) int {
+	total := 0
+
+	for _, r := range ranges {
+		nIngredients := r.End - r.Start + 1
+		// log.Printf("Range %+v has %d ingredients", r, nIngredients)
+		total += nIngredients
+	}
+
+	return total
+}
+
+func isFreshIngredient(id int, ranges []IngredientIDRange) bool {
+	for _, r := range ranges {
+		if id >= r.Start && id <= r.End {
+			return true
+		}
+	}
+
+	return false
+}
+
+func parseInput(filePath string) ([]IngredientIDRange, []int) {
+	lines := Utils.GetLinesFromFile(filePath)
+
+	ranges := []IngredientIDRange{}
+	ids := []int{}
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		} else if strings.Contains(line, "-") {
+			tokens := strings.Split(line, "-")
+			start := Utils.StringToInt(tokens[0])
+			end := Utils.StringToInt(tokens[1])
+
+			ranges = append(ranges, IngredientIDRange{Start: start, End: end})
+		} else {
+			ids = append(ids, Utils.StringToInt(line))
+		}
+	}
+
+	// Sorting ranges by its start value ensures merging works correctly otherwise merge order
+	// can lead to unexpected overlapping ranges
+	slices.SortFunc(ranges, func(r1, r2 IngredientIDRange) int {
+		return r1.Start - r2.Start
+	})
+
+	return ranges, ids
+}
--- aoc-2025/main.go
+++ aoc-2025/main.go
@@ -5,6 +5,7 @@ import (
 	Day2 "aoc-2025/day2"
 	Day3 "aoc-2025/day3"
 	Day4 "aoc-2025/day4"
+	Day5 "aoc-2025/day5"
 	"log"
 )
 
@@ -19,6 +20,7 @@ func main() {
 		&Day2.Day2{InputFilePath: "./input/day-2.txt"},
 		&Day3.Day3{InputFilePath: "./input/day-3.txt"},
 		&Day4.Day4{InputFilePath: "./input/day-4.txt"},
+		&Day5.Day5{InputFilePath: "./input/day-5.txt"},
 	}
 
 	log.Printf("Advent of Code 2025 Solutions")
